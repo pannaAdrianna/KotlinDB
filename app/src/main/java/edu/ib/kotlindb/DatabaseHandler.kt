495e73b1f046f8229fffcaed8f82ac4588512a8d
Adrianna Boczar <250319@student.pwr.edu.pl>
2021-05-25 01:10:04 +0200

    Initial commit

---
+++ app/src/main/java/edu/ib/kotlindb/DatabaseHandler.kt
@@ -0,0 +1,52 @@
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import edu.ib.kotlindb.EmpModelClass
+
+//creating the database logic, extending the SQLiteOpenHelper base class
+class DatabaseHandler(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private val DATABASE_VERSION = 1
+        private val DATABASE_NAME = "EmployeeDatabase"
+
+        private val TABLE_CONTACTS = "EmployeeTable"
+
+        private val KEY_ID = "_id"
+        private val KEY_NAME = "name"
+        private val KEY_EMAIL = "email"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        //creating table with fields
+        val CREATE_CONTACTS_TABLE = ("CREATE TABLE " + TABLE_CONTACTS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_EMAIL + " TEXT" + ")")
+        db?.execSQL(CREATE_CONTACTS_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_CONTACTS")
+        onCreate(db)
+    }
+
+    /**
+     * Function to insert data
+     */
+    fun addEmployee(emp: EmpModelClass): Long {
+        val db = this.writableDatabase
+
+        val contentValues = ContentValues()
+        contentValues.put(KEY_NAME, emp.name) // EmpModelClass Name
+        contentValues.put(KEY_EMAIL, emp.email) // EmpModelClass Email
+
+        // Inserting employee details using insert query.
+        val success = db.insert(TABLE_CONTACTS, null, contentValues)
+        //2nd argument is String containing nullColumnHack
+
+        db.close() // Closing database connection
+        return success
+    }
+}
---
+++ app/src/main/java/edu/ib/kotlindb/MainActivity.kt
@@ -0,0 +1,76 @@
+package edu.ib.kotlindb
+
+import DatabaseHandler
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+
+        // Click even of the add button.
+        btnAdd.setOnClickListener { view ->
+
+            addRecord()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    //Method for saving the employee records in database
+    private fun addRecord() {
+        val etName: EditText = findViewById(R.id.etName)
+        val etEmailId: EditText = findViewById(R.id.etEmailId)
+
+        val name = etName.text.toString()
+        val email = etEmailId.text.toString()
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        if (!name.isEmpty() && !email.isEmpty()) {
+            val status =
+                databaseHandler.addEmployee(EmpModelClass(0, name, email))
+            if (status > -1) {
+                Toast.makeText(applicationContext, "Record saved", Toast.LENGTH_LONG).show()
+                etName.text.clear()
+                etEmailId.text.clear()
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Name or Email cannot be blank",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+
+}
