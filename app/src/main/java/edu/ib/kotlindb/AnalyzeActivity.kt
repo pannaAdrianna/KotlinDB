3bc01cead58a9d909d6e7194d2d13d2ff0e962eb
Adrianna Boczar <250319@student.pwr.edu.pl>
2021-06-21 18:17:12 +0200

    Checking works

--- app/src/main/java/edu/ib/kotlindb/AnalyzeActivity.kt
+++ app/src/main/java/edu/ib/kotlindb/AnalyzeActivity.kt
@@ -2,20 +2,21 @@ package edu.ib.kotlindb
 
 import DatabaseHandler
 import android.os.Bundle
-import android.view.View
+import android.text.TextUtils
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.content_main.*
+
 
 class AnalyzeActivity : AppCompatActivity() {
 
     lateinit var etIngredients: EditText
     lateinit var tvResult: TextView
     internal lateinit var databaseHandler: DatabaseHandler
-
+    internal lateinit var empList: ArrayList<EmpModelClass>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +25,47 @@ class AnalyzeActivity : AppCompatActivity() {
 
         var etIngredients: EditText = findViewById(R.id.etINCIstr)
         var tvResult: TextView = findViewById(R.id.tvControversialngriedients)
-        databaseHandler = DatabaseHandler(this)
-
         val btnAnalyze = findViewById<Button>(R.id.btnAnalyze)
+        val results: MutableList<String> = ArrayList()
+
+        databaseHandler = DatabaseHandler(this)
+        empList = databaseHandler.viewEmployee()
+
+        etIngredients.text.clear()
+        results.clear()
+
+
         btnAnalyze.setOnClickListener { view ->
-            val test = etIngredients.getText().toString()
-            tvResult.setText(test);
-            etIngredients.text.clear()
+
+            if (TextUtils.isEmpty(etIngredients.getText())) {
+                Toast.makeText(this, "Add ingredients to verify", Toast.LENGTH_SHORT).show();
+                tvResult.setText("");
+            } else {
+                val test = etIngredients.text.toString()
+                tvResult.text = test;
+
+                val array = test.trim().split(",").toTypedArray()
+                Log.i("AnalyzeActivity", array[0].toString())
+                println(array)
+
+                for (ingToCheck in array) {
+                    println(ingToCheck)
+                    for (ingredient in empList) {
+                        var name = ingredient.label.trim()
+                        if (ingToCheck.equals(name, ignoreCase = true) and (name !in results)) {
+                                results.add(name)
+
+                        }
+                    }
+                }
+
+//                tvResult.text = results.toString();
+                val resultDisplay = StringBuilder()
+                for (str in results) resultDisplay.append("$str, ")
+                tvResult.text = resultDisplay
+
+            }
+
 
         }
     }
