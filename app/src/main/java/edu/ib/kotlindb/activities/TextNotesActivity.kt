2f79ed966e1cc4e3bee653c412b4a1564a0dd50d
pannaAdrianna <250319@student.pwr.edu.pl>
2021-11-24 19:06:28 +0100

    added floating button

--- app/src/main/java/edu/ib/kotlindb/activities/TextNotesActivity.kt
+++ app/src/main/java/edu/ib/kotlindb/activities/TextNotesActivity.kt
@@ -1,6 +1,7 @@
 package edu.ib.kotlindb.activities
 
 import DatabaseHandler
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,6 +11,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import edu.ib.kotlindb.R
 import edu.ib.kotlindb.database.EmpModelClass
 import edu.ib.kotlindb.database.TextNotesAdapter
@@ -22,6 +24,11 @@ class TextNotesActivity : AppCompatActivity() {
     internal lateinit var empList: ArrayList<TextNote>
 
     internal lateinit var rvItemsList: RecyclerView
+    internal lateinit var btnAdd: FloatingActionButton
+
+    val TAG: String = "TextNotesActivity: "
+
+
     internal var searchView: SearchView? = null
     val requestCSVcode = 1
     internal lateinit var myList: ArrayList<HashMap<String, String>>
@@ -35,12 +42,30 @@ class TextNotesActivity : AppCompatActivity() {
 
         // to z content
         rvItemsList = findViewById(R.id.rvTextNotes)
+        btnAdd = findViewById(R.id.btnAddNewNote)
         searchView = findViewById(R.id.search_bar)
         setupListofDataIntoRecyclerView()
         adapter = TextNotesAdapter(this, getBasicItemsList())
         rvItemsList.setAdapter(adapter);
 
+
+        btnAdd.setOnClickListener { view ->
+            Toast.makeText(
+                applicationContext,
+                TAG + " btn Add click",
+                Toast.LENGTH_LONG
+            ).show()
+
+
+
+            TODO("alert dialog")
+        }
+
     }
+
+
+
+
 
     private fun getBasicItemsList(): ArrayList<TextNote> {
         //creating the instance of DatabaseHandler class
@@ -56,24 +81,33 @@ class TextNotesActivity : AppCompatActivity() {
         rvItemsList = findViewById(R.id.rvTextNotes)
         val tvNoRecordsAvailable: TextView = findViewById(R.id.tvNoRecordsAvailable)
 
-//        if (getBasicItemsList().size == 0) {
-//            readFromCSV()
-//        }
-//        if (getPreggoItemsList().size == 0) {
-//            readFromCSV2()
-//        }
 
         if (getBasicItemsList().size == 0) {
-            rvItemsList.visibility = View.INVISIBLE
-        } else {
-            rvItemsList.visibility = View.VISIBLE
-            tvNoRecordsAvailable.visibility = View.GONE
+            rvItemsList.visibility = View.GONE
+            tvNoRecordsAvailable.visibility = View.VISIBLE
+            addSomeData()
         }
+
+        rvItemsList.visibility = View.VISIBLE
+        tvNoRecordsAvailable.visibility = View.GONE
+
         rvItemsList.layoutManager = LinearLayoutManager(this)
         val itemAdapter = TextNotesAdapter(this, getBasicItemsList())
         rvItemsList.adapter = itemAdapter
 
 
+    }
+
+    private fun addSomeData() {
+        databaseHandler.addTextNote(
+            TextNote(1, "Title Test 1", "DESC test 1")
+        )
+        databaseHandler.addTextNote(
+            TextNote(2, "Title Test 2", "DESC test 2")
+        )
+        databaseHandler.addTextNote(
+            TextNote(3, "Title Test 3", "DESC test 3")
+        )
     }
 
     fun deleteRecordAlertDialog(empModelClass: TextNote) {
@@ -119,6 +153,8 @@ class TextNotesActivity : AppCompatActivity() {
         alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
         alertDialog.show()  // show the dialog to UI
     }
+
+    fun onBtnAddNoteClick(view: android.view.View) {}
 
 
 }
